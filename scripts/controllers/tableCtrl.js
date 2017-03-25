c5f424cf916d40e0705f715f919d5cbea695a258
Miika Salo <miikasal@gmail.com>
2017-03-25 19:59:52 +0200

    Housekeeping

--- scripts/controllers/tableCtrl.js
+++ scripts/controllers/tableCtrl.js
@@ -5,8 +5,6 @@ app.controller('tableCtrl', ['$scope', '$http', function($scope, $http) {
 		angular.forEach($scope.tableItems, function(obj){
           obj.progress = 100 * (obj.sumNow - obj.sumProgressive) / obj.sumProgressive;
         });
-
-
 	});
 
 }]);
