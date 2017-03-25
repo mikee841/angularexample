9fbe27d3447fba39eba5ab9c4338de52ccc3bfbe
Miika Salo <miikasal@gmail.com>
2017-03-25 19:56:34 +0200

    Filter by name

--- scripts/controllers/tableCtrl.js
+++ scripts/controllers/tableCtrl.js
@@ -1,5 +1,12 @@
 app.controller('tableCtrl', ['$scope', '$http', function($scope, $http) {
 	$http.get('data/table.json').then(function(response) {
 		$scope.tableItems = response.data;
+
+		angular.forEach($scope.tableItems, function(obj){
+          obj.progress = 100 * (obj.sumNow - obj.sumProgressive) / obj.sumProgressive;
+        });
+
+
 	});
+
 }]);
